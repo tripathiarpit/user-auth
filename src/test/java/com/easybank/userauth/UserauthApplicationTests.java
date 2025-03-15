70be8153612e168c06c7e250f1c4b129f9c4f1dc
Arpit.Tripathi <arptripa@blueplanet.com>
2025-03-15 18:03:10 +0530

    first commit

---
+++ src/test/java/com/easybank/userauth/UserauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybank.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
